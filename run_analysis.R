5ef9246cacc39f88eae20153643dd094f522eeb3
Ahmed Hisham <etch7387@gmail.com>
2019-08-24 20:07:55 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,102 @@
+######################################### prepration
 
+library(dplyr)
+
+#Download the dataset
+
+filename <- "Coursera_DS3_Final.zip"
+
+# Checking if archieve already exists.
+setwd("E:/gethub path/getting and cleaning data - week 4 -course project")
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename)
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
+#Assigning all data frames
+
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+head(features)
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+head(activities)
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+head(subject_test)
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+head(x_test)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+head(y_test)
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+head(subject_train)
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+head(x_train)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+head(y_train)
+
+#####################################################################################
+# GO LIVE
+
+
+#Step 1: Merges the training and the test sets to create one data set.
+
+X <- rbind(x_train, x_test)
+head(X)
+Y <- rbind(y_train, y_test)
+head(Y)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+
+#Step 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+
+TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+head(TidyData)
+
+
+
+#Step 3: Uses descriptive activity names to name the activities in the data set.
+
+TidyData$code <- activities[TidyData$code, 2]
+head(TidyData)
+
+
+
+#Step 4: Appropriately labels the dats(TidyData))
+
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+
+
+#Step 5: From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+
+
+#Checking variable names
+
+str(FinalData)
+
+#Take a look at final data
+
+FinalData
